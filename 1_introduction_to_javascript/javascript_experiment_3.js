d695ea58356c0bf94c5fe39544fdcd29f639d8f1
Kalulu <22106318@student.westernsydney.edu,au>
2025-01-18 18:30:47 +0700

    d

--- 1_introduction_to_javascript/javascript_experiment_3.js
+++ 1_introduction_to_javascript/javascript_experiment_3.js
@@ -1,9 +1,13 @@
 // DATE: SATURDAY 18 JAN 2025
-window.age = 10;
 
+// LESSON: this
+
+var 
 function Person() {
     this.age = 42;
     setTimeout(() => {
         console.log('this.age', this.age);
     }, 1000);
-}
+};
+
+console.log(Person());
